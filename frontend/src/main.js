f65f33ca48ef6018c228dfa69bb6ffe278b8eb9d
Noe Gonzalez <gnoe234@gmail.com>
2019-02-03 20:56:32 -0600

    worked on getting base login page

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,9 +2,20 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import Vuetify from 'vuetify'
 import router from './router'
 
 Vue.config.productionTip = false
+
+import 'vuetify/dist/vuetify.min.css'
+
+Vue.use(Vuetify, {
+  theme: {
+    primary: '#3f51b5',
+    secondary: '#b0bec5',
+    accent: '#8c9eff',
+    error: '#b71c1c'
+  }});
 
 /* eslint-disable no-new */
 new Vue({
