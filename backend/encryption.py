c5156721c87cbf361185461a320e67b4e05b0405
Ian Siebold <iansiebold@gmail.com>
2019-02-05 18:38:42 -0600

    Base commit for my class

---
+++ backend/encryption.py
@@ -0,0 +1,12 @@
+class encryption:
+    @staticmethod
+    def encrypt(key, message):
+        return ''
+
+    @staticmethod
+    def decrypt(key, message):
+        return ''
+
+    @staticmethod
+    def generateKeys():
+        return ''
