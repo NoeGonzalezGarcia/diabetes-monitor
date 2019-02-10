fe6dfc0e858f3bca06856d0dc7a81f09515f89b1
Ian <iansiebold@gmail.com>
2019-02-10 12:44:49 -0600

    Just needs to be tested and converted into a class

--- backend/encryption.py
+++ backend/encryption.py
@@ -4,11 +4,15 @@ import random
 MAXNUM = 1000000
 LOWESTN = 1000
 
-def encrypt(key, message):
-    return ''
+#https://gist.github.com/JonCooperWorks/5314103
+def encrypt(key, n, message):
+    text = [chr((char) ** key) % n for char in message]
+    return text
 
-def decrypt(key, message):
-    return ''
+#https://gist.github.com/JonCooperWorks/5314103
+def decrypt(key, n, message):
+    text = [chr((char) ** key) % n for char in message]
+    return ''.join(text)
 
 def generateKeys():
     n = 1
@@ -19,13 +23,46 @@ def generateKeys():
             p2 = getLargePrime(MAXNUM)
         n = p1 * p2
 
-    nn = lcm(p1 - 1, p2 - 1)
-    e = 3
+    nn = (p1 - 1) * (p2 - 1)
+    e = random.randrange(1, nn)
 
-    #This is not how you make a private key. The internet is confusing me.
-    d = math.pow(e, -1)%nn
+    g = gcd(1, nn)
+    while g != 1:
+        e = random.randrange( 1, nn)
+        g = gcd(e, nn)
+
+    d = multiplicative_inverse(e, nn)
         
     return n, e, d
+
+#https://gist.github.com/JonCooperWorks/5314103
+#Used to find the private key 
+def multiplicative_inverse(e, nn):
+    d = 0
+    x1 = 0
+    x2 = 1
+    y1 = 1
+    temp_nn = nn
+
+    while e > 0:
+        temp 1 = temp_nn / e
+        temp2 = temp_nn - temp1 * e
+        temp_nn = e
+        e = temp2
+        x = x2 - temp1 * x1
+        y = d - temp1 * y2
+        x2 = x1
+        x1 = x
+        d = y1
+        y1 = y
+
+    if temp_nn == 1
+    return d + nn
+
+def gcd(a, b):
+    while b != 0:
+        a, b = b, a % b
+    return a
 
 #https://inventwithpython.com/hacking/chapter23.html
 def getLargePrime(n):
