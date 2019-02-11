1ab22c3f06150f6519f390dfbc957cb779efb008
eric.albrecht <albrechter@ad.msoe.edu>
2019-02-11 15:16:51 -0600

    adds heartbeat server that tracks status of server online or not

---
+++ backend/heartbeatserver/main.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+import requests
+from datetime import datetime
+from flask import Flask, flash, redirect, render_template, request, url_for, abort
+import time
+
+app = Flask(__name__)
+
+
+def main():
+    while 1 == 1:
+        time.sleep(2)
+        try:
+            r = requests.get('http://127.0.0.1:5000/heartbeat1')
+            if r.content == b'working':
+                print("Connected: " + str(datetime.now()))
+            else:
+                print("INVALID RETURN DATA " + str(datetime.now()))
+        except Exception:
+            print("DISCONNECTED SYSTEM ADMIN ASSISTANCE NEEDED " + str(datetime.now()))
+
+
+main()
