14a9bd3c09073ca11422ae3067b6204f742c49cb
Ian <iansiebold@gmail.com>
2019-02-10 18:01:19 -0600

    Moved testing for encryption to its own file

--- backend/encryption.py
+++ backend/encryption.py
@@ -121,18 +121,3 @@ def lcm(x, y):
         greater += 1
 
     return lcm
-
-def main():
-    print("Generating keys")
-    publicCombo, privateCombo = generateKeys()
-    n, e = publicCombo
-    d, e = privateCombo
-    print("Public key: ", e, " Private key: ", d, " E value: ", n)
-    message = input("Enter a message to encrypt: ")
-    encrypted = encrypt(e, n, message)
-    print("Encrypted message: ", encrypted)
-    decrypted = decrypt(d, n, encrypted)
-    print("Decrypted message: ", decrypted)
-
-if __name__ == '__main__':
-    main()
