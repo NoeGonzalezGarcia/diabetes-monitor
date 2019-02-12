d8871f7f893c2946edc7a61a7a9487cd7aa38c0e
Noe Gonzalez <gnoe234@gmail.com>
2019-02-12 00:36:54 -0600

    One more

--- frontend/src/components/view-data.vue
+++ frontend/src/components/view-data.vue
@@ -46,7 +46,7 @@ export default {
             Getget() {
                 axios.get(`http://127.0.0.1:5000/get_data/`+ new Date().toString()+ '/Breakfast')
                 .then((response) => {
-                this.resp = response
+                this.resp = response.value
                 })
                 .catch(e => {
                     this.errors.push(e)
