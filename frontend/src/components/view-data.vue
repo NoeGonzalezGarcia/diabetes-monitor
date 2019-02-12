99c59cb00fe3c05e387e30f8af026151953f9a62
Noe Gonzalez <gnoe234@gmail.com>
2019-02-12 01:05:19 -0600

    changing to inner function

--- frontend/src/components/view-data.vue
+++ frontend/src/components/view-data.vue
@@ -46,7 +46,7 @@ export default {
             Getget() {
                 var self = this
                 axios.get(`http://127.0.0.1:5000/get_data/`+ new Date().toString()+ '/Breakfast')
-                .then((response) => {
+                .then(function (response) {
                 self.resp = response.data
                 })
                 .catch(e => {
