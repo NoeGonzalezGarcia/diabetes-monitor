4b1e8dac4a3183cbcfdd82454a8522a463b5c819
eric.albrecht <albrechter@ad.msoe.edu>
2019-02-12 14:35:33 -0600

    updates to add text notification server via twilio

--- backend/heartbeatserver/main.py
+++ backend/heartbeatserver/main.py
@@ -3,29 +3,31 @@ import requests
 from datetime import datetime
 from flask import Flask, flash, redirect, render_template, request, url_for, abort
 import time
+import sms
 import smtplib, ssl
 
 app = Flask(__name__)
 
 
 def main():
-    email_sent = False
+    text_sent = False
+    first_connect = False
     while 1 == 1:
-        time.sleep(2)
+        time.sleep(10)
         try:
             r = requests.get('http://127.0.0.1:5000/heartbeat')
             if r.content == b'working':
                 print("Connected: " + str(datetime.now()))
-                email_sent = False
+                text_sent = False
+                firstConnect = True
             else:
                 print("INVALID RETURN DATA " + str(datetime.now()))
         except Exception:
             print("DISCONNECTED SYSTEM ADMIN ASSISTANCE NEEDED " + str(datetime.now()))
-            if not email_sent:
-                # send_sys_admin_email()
-                email_sent = True
-
-
+            if not text_sent and not first_connect:
+                sms.send_sys_admin_text()
+                send_email.send_sys_admin_email()
+                text_sent = True
 
 
 main()
