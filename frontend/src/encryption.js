cb354e537c133067ef97203cad8d73ad7aa452ad
Noe Gonzalez <gnoe234@gmail.com>
2019-02-11 21:30:58 -0600

    CHanged encryption to js

--- frontend/src/encryption.js
+++ frontend/src/encryption.js
@@ -1,88 +1,96 @@
-import math
-import random
+const maxnum = 1000
+const minnum = 101
+const lowestn = 1000
 
-MAXNUM = 10000
-#MINNUM must be odd
-MINNUM = 1001
-LOWESTN = 1000
-
-def encrypt(key, n, message):
+function encrypt(key, n, message){
     text = []
-    for char in message:
-        i = ord(char) #character to unicode
-        j = pow(i, key, n)
-        text.append(j)
+    for (i = 0; i < message.length; i++){
+        val = message.charCodeAt(i)
+        j = Math.pow(i,key)%n
+        text[i] = j
+    }
     return text
-
-def decrypt(key, n, message):
+}
+function decrypt(key, n, message){
     text = []
-    for char in message:
-        i = pow(char, key, n)
-        j = chr(i)
-        text.append(j)
-    return ''.join(text)
+    for (i = 0; i < message.length; i++){
+        val = Math.pow(i,key)%n
+        j = String.fromCharCode(val)
+        text[i] = j;
+    }
+    return text.join("")
+}
 
-def generateKeys():
+function generateKeys(){
     k = 2
-    while is_Prime(k):
+    while(!isPrime()){
         n = 1
-        while n < LOWESTN:
-            p1 = getLargePrime(MAXNUM)
-            p2 = getLargePrime(MAXNUM)
-            while p1 == p2:
-                p2 = getLargePrime(MAXNUM)
+        while(n < lowestn){
+            p1 = getLargePrime(maxnum)
+            p2 = getLargePrime(maxnum)
+            while(p1 === p2){
+                p2 = getLargePrime(maxnum)
+            }
             n = p1 * p2
-
-        print("P1: ", p1)
-        print("P2: ", p2)
-        nn = (p1 - 1) * (p2 - 1)
-    
-        k = nn + 1
-        
-        d, e = mi2(k)
-
-    return (n), (d, e)
-
-def mi2(v):
+        }
+        nn = (p1 - 1) * (p2 -1)
+        k = nn++
+        de = mi2(k)
+    }
+    return [n, de]
+}
+function mi2(v){
     n = v
     factors = []
-    while n % 2 == 0:
-        factors.append(2)
-        n = n / 2
-    for i in range(3, int(math.sqrt(n)) + 1, 2):
-        while n % i == 0:
-            factors.append(i)
-            n = n / i
-    if n > 2:
-        factors.append(n)
-
+    i  = 0
+    while(n%2 === 0){
+        factors[i] = 2
+        n = n/2
+        i++
+    }
+    for(j = 3; parseInt(math.sqrt(n), 10) +1; j+=2){
+        while(n%j === 0){
+            factors[i] = j
+            n = n/j
+            i++
+        }
+    }
+    if(n > 2){
+        factors[i] = n
+    }
     d = 1
-    for x in factors:
-        d *= x
-    e = factors[len(factors) - 1]
+    for(k = 0; k < factors.length; k++){
+        d*=x
+    }
+    e = factors[factors.length - 1]
     d /= e
-
-    return int(d), int(e)
-
-def gcd(a, b):
-    while b != 0:
-        a, b = b, a % b
-    return a
-
-#https://inventwithpython.com/hacking/chapter23.html
-def getLargePrime(n):
-    isPrime = False
-    while not isPrime:
-        possibleNum = random.choice(range(MINNUM, MAXNUM, 2))
+    return [parseInt(d), parseInt(e)]
+}
+function gcd(a,b){
+    return gcd(a,b)
+}
+function getLargePrime(n){
+    isPrime = false
+    while(!isPrime){
+        possibleNum = Math.random(minnum, maxnum)
+        if(possibleNum%2 === 0){
+            possibleNum++;
+        }
         isPrime = is_Prime(possibleNum)
+    }
     return possibleNum
-
-def is_Prime(number):
-    if number == 2:
-        return True
-    if number < 2 or number % 2 == 0:
-        return False
-    for n in range(3, int(number ** 0.5) + 2, 2):
-        if number % n == 0:
-            return False
-    return True
+}
+function is_Prime(number){
+    if(number === 2){
+        return true
+    }
+    if(number < 2 || number %2 ===0){
+        return false
+    }
+    for(i = 3; i < parseInt(Math.pow(number, 0.5) +2); i++){
+        if(number %n === 0){
+            return false
+        }
+    }
+    return true
+}
